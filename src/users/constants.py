1999e79b9a8afa9f360d8e917aa988562571f361
RaviBhalani <ravibhalani8@gmail.com>
2026-06-20 23:02:14 +0530

    feature/user
    [*] Reduce length of hashed password to 60 as it can't exceed 60 for any length of input with bcrypt.

--- src/users/constants.py
+++ src/users/constants.py
@@ -8,5 +8,5 @@ class UserRole(str, Enum):
 
 NAME_MAX_LENGTH = 150
 EMAIL_MAX_LENGTH = 255
-PASSWORD_MAX_LENGTH = 255
+PASSWORD_MAX_LENGTH = 60
 ROLE_MAX_LENGTH = max(len(role.value) for role in UserRole)
