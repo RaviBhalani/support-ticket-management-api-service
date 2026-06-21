9555a1a6792cf700e3678ae86e7cdb14fd9b4a9b
RaviBhalani <ravibhalani8@gmail.com>
2026-06-21 22:10:52 +0530

    feature/ticket
    [+] Add exceptions for the ticket update endpoint.

--- src/tickets/exceptions.py
+++ src/tickets/exceptions.py
@@ -1,7 +1,12 @@
 from starlette import status
 
 from src.core.exceptions import AppException
-from src.tickets.constants import CUSTOMER_NOT_FOUND_MSG, INVALID_CUSTOMER_ROLE_MSG
+from src.tickets.constants import (
+    CUSTOMER_NOT_FOUND_MSG,
+    INVALID_CUSTOMER_ROLE_MSG,
+    STATUS_CHANGE_NOT_ALLOWED_MSG,
+    TICKET_NOT_FOUND_MSG,
+)
 
 
 class CustomerNotFoundError(AppException):
@@ -12,3 +17,13 @@ class CustomerNotFoundError(AppException):
 class InvalidCustomerRoleError(AppException):
     status_code = status.HTTP_422_UNPROCESSABLE_CONTENT
     detail = INVALID_CUSTOMER_ROLE_MSG
+
+
+class TicketNotFoundError(AppException):
+    status_code = status.HTTP_404_NOT_FOUND
+    detail = TICKET_NOT_FOUND_MSG
+
+
+class StatusChangeNotAllowedError(AppException):
+    status_code = status.HTTP_403_FORBIDDEN
+    detail = STATUS_CHANGE_NOT_ALLOWED_MSG
