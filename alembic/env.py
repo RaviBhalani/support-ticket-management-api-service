add634f821973f20766fa031dd4ec9f9db20e73e
RaviBhalani <ravibhalani8@gmail.com>
2026-06-21 17:12:30 +0530

    feature/ticket
    [*] Do cosmetic change in variable name in env.py due to corresponding connection url name change.

--- alembic/env.py
+++ alembic/env.py
@@ -16,7 +16,7 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-config.set_main_option("sqlalchemy.url", settings.db.url)
+config.set_main_option("sqlalchemy.url", settings.db.async_url)
 
 target_metadata = Base.metadata
 
