19019d6c65e7b9fef8ce104f0611e3f916ad0013
RaviBhalani <ravibhalani8@gmail.com>
2026-06-21 21:14:48 +0530

    feature/ticket
    [*] Replace unprocessable entity to unprocessable content as the former is deprecated.

--- src/tickets/exceptions.py
+++ src/tickets/exceptions.py
@@ -5,10 +5,10 @@ from src.tickets.constants import CUSTOMER_NOT_FOUND_MSG, INVALID_CUSTOMER_ROLE_
 
 
 class CustomerNotFoundError(AppException):
-    status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
+    status_code = status.HTTP_422_UNPROCESSABLE_CONTENT
     detail = CUSTOMER_NOT_FOUND_MSG
 
 
 class InvalidCustomerRoleError(AppException):
-    status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
+    status_code = status.HTTP_422_UNPROCESSABLE_CONTENT
     detail = INVALID_CUSTOMER_ROLE_MSG
